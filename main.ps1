d93f5f0c213568cc0dbe597b3e03c59110514a28
flips991 <102617171+flips991@users.noreply.github.com>
2023-12-06 20:31:51 +0100

    Create main.ps1

---
+++ main.ps1
@@ -0,0 +1,20 @@
+# Define an array of script URLs
+$scriptUrls = @(
+    "https://raw.githubusercontent.com/flips991/winauto/main/pwandkeyset.ps1"
+    "https://raw.githubusercontent.com/asheroto/winget-install/master/winget-install.ps1",
+    "https://raw.githubusercontent.com/flips991/winauto/main/programs.ps1"
+)
+
+# Loop through each script URL and execute it
+foreach ($scriptUrl in $scriptUrls) {
+    Write-Host "Downloading and executing script from $scriptUrl"
+    
+    # Download the script content
+    $scriptContent = Invoke-RestMethod -Uri $scriptUrl
+    
+    # Create a script block and execute it
+    $scriptBlock = [ScriptBlock]::Create($scriptContent)
+    & $scriptBlock -Force
+
+    Write-Host "Script execution complete."
+}
