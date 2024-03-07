e05723e572acd6f0fc5356f0025dbbdd400bba94
flips991 <102617171+flips991@users.noreply.github.com>
2024-03-07 09:32:26 +0100

    Update programs.ps1
    Updated chrome command for installing on all users

--- programs.ps1
+++ programs.ps1
@@ -2,7 +2,7 @@
 Write-Host "Installing Programs"
 
 winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements
-winget install -e --id Google.Chrome
+winget install --scope machine -e --id Google.Chrome
 winget install -e --id Mozilla.Firefox
 winget install -e --id 7zip.7zip
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
