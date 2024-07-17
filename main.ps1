7b3f8e5c409316bfce958b719bc89cade583cae7
flips991 <102617171+flips991@users.noreply.github.com>
2024-07-17 15:15:22 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,7 +1,7 @@
 # Define an array of script URLs
 $scriptUrls = @(
     "https://raw.githubusercontent.com/flips991/winauto/main/pwandkeyset.ps1"
-    "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/winget.ps1",
+    # "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/winget.ps1",
     "https://raw.githubusercontent.com/flips991/winauto/main/programs.ps1"
 )
 
