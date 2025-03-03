46a43ef8087a4045c9e2c7b72bc7d348f15cff47
flips991 <102617171+flips991@users.noreply.github.com>
2025-03-03 15:13:50 +0100

    Update programs.ps1

--- programs.ps1
+++ programs.ps1
@@ -4,7 +4,7 @@ Write-Host "Installing Programs"
 winget install -e --id Adobe.Acrobat.Reader.64-bit --accept-source-agreements
 winget install --scope machine -e --id Google.Chrome
 winget install -e --id Mozilla.Firefox
-winget install -e --id 7zip.7zip
+# winget install -e --id 7zip.7zip
 winget install -e --id AnyDeskSoftwareGmbH.AnyDesk
 winget install -e --id VideoLAN.VLC
 
